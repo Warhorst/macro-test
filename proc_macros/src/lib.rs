c8be357b2b5a353afc94ba7f7fb77d050535d56b
Warhorst <weigelt.johannes@t-online.de>
2022-04-29 14:17:45 +0200

    Created prototype of assertion macro. Currently not functional.

---
+++ proc_macros/src/lib.rs
@@ -0,0 +1,73 @@
+use proc_macro::TokenStream;
+use std::ops::Deref;
+use quote::quote;
+use syn::{FnArg, Ident, ItemFn, parse, ReturnType, Signature, Type};
+
+#[proc_macro_attribute]
+pub fn testable_proc_macro_attribute(_attributes: TokenStream, item: TokenStream) -> TokenStream {
+    if let Ok(item_func) = parse::<ItemFn>(item) {
+        if !signature_as_expected(&item_func.sig) {
+            panic!("'testable_proc_macro_attribute' is only applicable on functions of type (AttributeArgs, TokenStream2) -> TokenStream2")
+        }
+
+        let ident = &item_func.sig.ident;
+        let new_ident = Ident::new(&format!("{}_testable", ident.to_string()), ident.span());
+        let block = &item_func.block;
+        let params = &item_func.sig.inputs;
+        let output = &item_func.sig.output;
+
+        let result = quote! {
+            #[proc_macro_attribute]
+            pub fn #ident (attributes: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+                #new_ident(
+                    syn::parse_macro_input!(attributes as AttributeArgs),
+                    item.into()
+                ).into()
+            }
+
+            fn #new_ident (#params) #output {
+                #block
+            }
+        };
+
+        return result.into()
+    }
+
+    panic!("'testable_proc_macro_attribute' is only allowed on functions")
+}
+
+fn signature_as_expected(sig: &Signature) -> bool {
+    if !sig.inputs.len() == 2 {
+        // only two arguments allowed
+        return false;
+    }
+
+    let first_param_ok = match &sig.inputs[0] {
+        FnArg::Receiver(_) => false,
+        FnArg::Typed(typed) => if let Type::Path(p) = typed.ty.deref() {
+            p.path.get_ident().unwrap().to_string().ends_with("AttributeArgs")
+        } else {
+            false
+        }
+    };
+
+    let second_param_ok = match &sig.inputs[1] {
+        FnArg::Receiver(_) => false,
+        FnArg::Typed(typed) => if let Type::Path(p) = typed.ty.deref() {
+            p.path.get_ident().unwrap().to_string().ends_with("TokenStream2")
+        } else {
+            false
+        }
+    };
+
+    let output_ok = match &sig.output {
+        ReturnType::Default => false,
+        ReturnType::Type(_, ty) => if let Type::Path(p) = ty.deref() {
+            p.path.get_ident().unwrap().to_string().ends_with("TokenStream2")
+        } else {
+            false
+        }
+    };
+
+    first_param_ok && second_param_ok && output_ok
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,108 @@
-use proc_macro::TokenStream;
-use std::ops::Deref;
-use quote::quote;
-use syn::{FnArg, Ident, ItemFn, parse, ReturnType, Signature, Type};
+use syn::__private::TokenStream2;
+use syn::{Attribute, AttributeArgs, Ident, Item, Path};
+use syn::Item::*;
 
-#[proc_macro_attribute]
-pub fn testable_proc_macro_attribute(attributes: TokenStream, item: TokenStream) -> TokenStream {
-    if let Ok(item_func) = parse::<ItemFn>(item) {
-        if !signature_as_expected(&item_func.sig) {
-            panic!("'testable_proc_macro_attribute' is only applicable on functions of type (AttributeArgs, TokenStream2) -> TokenStream2")
+mod inner_mod {
+    pub mod implementation {
+        use syn::__private::TokenStream2;
+        use syn::AttributeArgs;
+
+        pub fn bar(_attr: AttributeArgs, item: TokenStream2) -> TokenStream2 {
+            item
         }
-
-        let ident = &item_func.sig.ident;
-        let new_ident = Ident::new(&format!("{}_testable", ident.to_string()), ident.span());
-        let block = &item_func.block;
-        let params = &item_func.sig.inputs;
-        let output = &item_func.sig.output;
-
-        let result = quote! {
-            #[proc_macro_attribute]
-            pub fn #ident (attributes: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-                #new_ident(
-                    syn::parse_macro_input!(attributes as AttributeArgs),
-                    item.into()
-                ).into()
-            }
-
-            fn #new_ident (#params) #output {
-                #block
-            }
-        };
-
-        return result.into()
     }
-
-    panic!("'testable_proc_macro_attribute' is only allowed on functions")
 }
 
-fn signature_as_expected(sig: &Signature) -> bool {
-    if !sig.inputs.len() == 2 {
-        // only two arguments allowed
-        return false;
+#[macro_export]
+macro_rules! assert_attribute_implementation_as_expected {
+    ($base_path:path : $attr:ident, item: {$item:item}  expected: {$($expected:tt)*}) => {
+        {
+            use $base_path :: {implementation :: $attr};
+
+            let ident = syn::parse2::<syn::Ident>(quote::quote! {$attr}).unwrap();
+            let mut item = syn::parse2::<syn::Item>(quote::quote! { $item }).unwrap();
+            let expected_ts = quote::quote! { $($expected)* }.to_string();
+            let attribute = crate::extract_attribute_from_item(&ident, &mut item);
+            let attr_args = crate::extract_attribute_args(attribute);
+            let implementation_ts = $attr(attr_args, quote::quote! { $item }).to_string();
+            crate::assert_tokens_are_equal(implementation_ts, expected_ts)
+        }
     }
+}
 
-    let first_param_ok = match &sig.inputs[0] {
-        FnArg::Receiver(_) => false,
-        FnArg::Typed(typed) => if let Type::Path(p) = typed.ty.deref() {
-            p.path.get_ident().unwrap().to_string().ends_with("AttributeArgs")
-        } else {
-            false
-        }
-    };
+fn extract_attribute_from_item(ident: &Ident, item: &mut Item) -> Attribute {
+    let mut attributes = get_attributes_from_item(item);
+    let attribute_index = attributes.iter()
+        .enumerate()
+        .find(|(_, a)| attribute_has_ident(a, ident))
+        .expect("Could not find expected attribute").0;
+    attributes.remove(attribute_index)
+}
 
-    let second_param_ok = match &sig.inputs[1] {
-        FnArg::Receiver(_) => false,
-        FnArg::Typed(typed) => if let Type::Path(p) = typed.ty.deref() {
-            p.path.get_ident().unwrap().to_string().ends_with("TokenStream2")
-        } else {
-            false
-        }
-    };
+fn get_attributes_from_item(item: &mut Item) -> &mut Vec<Attribute> {
+    match item {
+        Const(i) => &mut i.attrs,
+        Enum(i) => &mut i.attrs,
+        ExternCrate(i) => &mut i.attrs,
+        Fn(i) => &mut i.attrs,
+        ForeignMod(i) => &mut i.attrs,
+        Impl(i) => &mut i.attrs,
+        Macro(i) => &mut i.attrs,
+        Macro2(i) => &mut i.attrs,
+        Mod(i) => &mut i.attrs,
+        Static(i) => &mut i.attrs,
+        Struct(i) => &mut i.attrs,
+        Trait(i) => &mut i.attrs,
+        TraitAlias(i) => &mut i.attrs,
+        Type(i) => &mut i.attrs,
+        Union(i) => &mut i.attrs,
+        Use(i) => &mut i.attrs,
+        _ => panic!("Could not extract attributes")
+    }
+}
 
-    let output_ok = match &sig.output {
-        ReturnType::Default => false,
-        ReturnType::Type(_, ty) => if let Type::Path(p) = ty.deref() {
-            p.path.get_ident().unwrap().to_string().ends_with("TokenStream2")
-        } else {
-            false
-        }
-    };
+pub fn attribute_has_ident(attribute: &Attribute, ident: &Ident) -> bool {
+    path_to_name(&attribute.path) == ident.to_string()
+}
 
-    first_param_ok && second_param_ok && output_ok
+fn path_to_name(path: &Path) -> String {
+    path.segments.last().map(|seg| seg.ident.to_string()).expect("The given path was not an identifier.")
+}
+
+fn extract_attribute_args(attr: Attribute) -> AttributeArgs {
+    match attr.parse_meta().unwrap() {
+        syn::Meta::List(list) => list.nested.into_iter().collect(),
+        _ => vec![]
+    }
+}
+
+pub fn assert_tokens_are_equal<L, R>(left: L, right: R) where L: AsRef<str>, R: AsRef<str> {
+    assert_eq!(remove_whitespace(left.as_ref()), remove_whitespace(right.as_ref()))
+}
+
+fn remove_whitespace(string: &str) -> String {
+    string.chars().filter(|c| !c.is_whitespace()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn foo() {
+        assert_attribute_implementation_as_expected!(
+            crate::inner_mod : bar,
+            item: {
+                #[bar]
+                struct S {
+                    foo: usize,
+                }
+            }
+
+            expected: {
+                #[bar]
+                struct S {
+                    foo: usize,
+                }
+            }
+        )
+    }
 }
