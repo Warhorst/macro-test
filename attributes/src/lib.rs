31b4b10d0b2f1ff5f3675baf2e80760a93c98748
Warhorst <weigelt.johannes@t-online.de>
2022-04-29 18:43:45 +0200

    Renamed attribute to proc_macro_attribute2, following the syn way.
    Added documentation to assert_attribute_implementation_as_expected.
    Refactorings.

---
+++ attributes/src/lib.rs
@@ -0,0 +1,77 @@
+use proc_macro::TokenStream;
+use std::ops::Deref;
+use quote::quote;
+use syn::{FnArg, ItemFn, parse, ReturnType, Signature, Type};
+use syn::__private::TokenStream2;
+
+#[proc_macro_attribute]
+pub fn proc_macro_attribute2(_attributes: TokenStream, item: TokenStream) -> TokenStream {
+    let item_func = parse::<ItemFn>(item).expect("'proc_macro_attribute2' is only allowed on functions");
+    implement(item_func).into()
+}
+
+fn implement(item_func: ItemFn) -> TokenStream2 {
+    if !signature_as_expected(&item_func.sig) {
+        panic!("'testable_proc_macro_attribute' is only applicable on functions of type (AttributeArgs, TokenStream2) -> TokenStream2")
+    }
+
+    let ident = &item_func.sig.ident;
+    let block = &item_func.block;
+    let params = &item_func.sig.inputs;
+    let output = &item_func.sig.output;
+
+    quote! {
+        #[proc_macro_attribute]
+        pub fn #ident (attributes: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+            implementation::#ident(
+                syn::parse_macro_input!(attributes as AttributeArgs),
+                item.into()
+            ).into()
+        }
+
+        pub mod implementation {
+            fn #ident (#params) #output {
+                #block
+            }
+        }
+    }
+}
+
+fn signature_as_expected(sig: &Signature) -> bool {
+    if sig.inputs.len() != 2 {
+        return false;
+    }
+
+    let first_param_ok = argument_of_expected_type(&sig.inputs[0], "AttributeArgs");
+    let second_param_ok = argument_of_expected_type(&sig.inputs[1], "TokenStream2");
+    let output_ok = output_of_expected_type(&sig.output);
+    first_param_ok && second_param_ok && output_ok
+}
+
+fn argument_of_expected_type(input: &FnArg, expected_type_name: &str) -> bool {
+    match input {
+        FnArg::Typed(typed) => match typed.ty.deref() {
+            Type::Path(p) => p.path.segments
+                .last()
+                .map(|seg| &seg.ident)
+                .map(|ident| ident.to_string() == expected_type_name)
+                .unwrap_or(false),
+            _ => false
+        }
+        _ => false,
+    }
+}
+
+fn output_of_expected_type(output: &ReturnType) -> bool {
+    match output {
+        ReturnType::Type(_, ty) => match ty.deref() {
+            Type::Path(p) => p.path.segments
+                .last()
+                .map(|seg| &seg.ident)
+                .map(|ident| ident.to_string() == "TokenStream2")
+                .unwrap_or(false),
+            _ => false,
+        }
+        _ => false,
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,48 @@
-use syn::__private::TokenStream2;
 use syn::{Attribute, AttributeArgs, Ident, Item, Path};
+use syn::__private::TokenStream2;
 use syn::Item::*;
 
-mod inner_mod {
-    pub mod implementation {
-        use syn::__private::TokenStream2;
-        use syn::AttributeArgs;
-
-        pub fn bar(_attr: AttributeArgs, item: TokenStream2) -> TokenStream2 {
-            item
-        }
-    }
-}
-
+/// This macro checks if an item with an attribute to test generates the
+/// expected token stream.
+/// This only works if your attributes uses the 'proc_macro_attribute2' attribute.
+/// Explanation below.
+///
+/// # How it works
+/// Imagine you have created an attribute which implements a static method for a struct.
+/// Maybe it creates a function that returns 42. To check if it works, use the macro like this:
+///
+/// ``` text
+/// assert_attribute_implementation_as_expected!(
+///             crate::my_attribute : create_the_answer,
+///             item: {
+///                 #[create_the_answer]
+///                 struct S {
+///                     foo: usize,
+///                 }
+///             }
+///
+///             expected: {
+///                 struct S {
+///                     foo: usize,
+///                 }
+///
+///                 impl S {
+///                     fn get_the_answer() -> usize {
+///                         42
+///                     }
+///                 }
+///             }
+///         )
+/// ```
+///
+/// Currently, a check is created whether the attribute ('create_the_answer' in this case)
+/// creates the same token stream as the input of expected. Both token streams are turned
+/// into strings and are checked for equality.
+///
+/// 'crate::my_attribute : create_the_answer' tells where your attribute is and what its named.
+/// The single colon is crucial because the path to the testable code will be in
+/// 'crate::my_attribute::implementation::create_the_answer'. This implementation module
+/// is created by 'proc_macro_attribute2'.
 #[macro_export]
 macro_rules! assert_attribute_implementation_as_expected {
     ($base_path:path : $attr:ident, item: {$item:item}  expected: {$($expected:tt)*}) => {
@@ -21,17 +51,17 @@ macro_rules! assert_attribute_implementation_as_expected {
 
             let ident = syn::parse2::<syn::Ident>(quote::quote! {$attr}).unwrap();
             let mut item = syn::parse2::<syn::Item>(quote::quote! { $item }).unwrap();
-            let expected_ts = quote::quote! { $($expected)* }.to_string();
+            let expected_ts = quote::quote! { $($expected)* };
             let attribute = crate::extract_attribute_from_item(&ident, &mut item);
             let attr_args = crate::extract_attribute_args(attribute);
-            let implementation_ts = $attr(attr_args, quote::quote! { $item }).to_string();
+            let implementation_ts = $attr(attr_args, quote::quote! { #item });
             crate::assert_tokens_are_equal(implementation_ts, expected_ts)
         }
     }
 }
 
 fn extract_attribute_from_item(ident: &Ident, item: &mut Item) -> Attribute {
-    let mut attributes = get_attributes_from_item(item);
+    let attributes = get_attributes_from_item(item);
     let attribute_index = attributes.iter()
         .enumerate()
         .find(|(_, a)| attribute_has_ident(a, ident))
@@ -76,20 +106,29 @@ fn extract_attribute_args(attr: Attribute) -> AttributeArgs {
     }
 }
 
-pub fn assert_tokens_are_equal<L, R>(left: L, right: R) where L: AsRef<str>, R: AsRef<str> {
-    assert_eq!(remove_whitespace(left.as_ref()), remove_whitespace(right.as_ref()))
+pub fn assert_tokens_are_equal(left: TokenStream2, right: TokenStream2) {
+    assert_eq!(remove_whitespace(left.to_string()), remove_whitespace(right.to_string()))
 }
 
-fn remove_whitespace(string: &str) -> String {
+fn remove_whitespace(string: String) -> String {
     string.chars().filter(|c| !c.is_whitespace()).collect()
 }
 
 #[cfg(test)]
 mod tests {
+    pub mod implementation {
+        use syn::__private::TokenStream2;
+        use syn::AttributeArgs;
+
+        pub fn bar(_attr: AttributeArgs, item: TokenStream2) -> TokenStream2 {
+            item
+        }
+    }
+
     #[test]
     fn foo() {
         assert_attribute_implementation_as_expected!(
-            crate::inner_mod : bar,
+            crate::tests : bar,
             item: {
                 #[bar]
                 struct S {
@@ -98,7 +137,6 @@ mod tests {
             }
 
             expected: {
-                #[bar]
                 struct S {
                     foo: usize,
                 }
