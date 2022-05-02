d00a70efd24b076394b61738b51b90d716f33764
Warhorst <weigelt.johannes@t-online.de>
2022-05-02 18:34:41 +0200

    Refactored macro (again) to generate less code. As a consequence, an import of a function provided by this crate is required.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+use quote::__private::Ident;
+use quote::quote;
+use syn::__private::TokenStream2;
+use syn::{Attribute, AttributeArgs, Item, Path};
 pub use attributes::proc_macro_attribute2;
 
 /// This macro checks if an item with an attribute to test generates the
-/// expected token stream.
+/// expected token stream. Requires the import of macro-test::compare_implementations.
 /// This only works if your attributes uses the 'proc_macro_attribute2' attribute.
 /// Explanation below.
 ///
@@ -45,67 +49,78 @@ pub use attributes::proc_macro_attribute2;
 macro_rules! assert_attribute_implementation_as_expected {
     ($base_path:path : $attr:ident, item: {$item:item}  expected: {$($expected:tt)*}) => {
         {
-            // Create the correct import for the attribute to test
             use $base_path :: {implementation :: $attr};
 
-            // Parse item and ident and transform the 'expected' token tree to a token stream
             let ident = syn::parse2::<syn::Ident>(quote::quote! {$attr}).unwrap();
-            let mut item = syn::parse2::<syn::Item>(quote::quote! { $item }).unwrap();
+            let item = syn::parse2::<syn::Item>(quote::quote! { $item }).unwrap();
             let expected_ts = quote::quote! { $($expected)* };
-
-            // Extract the attribute to test from the item
-            let attribute = {
-                let attribute_has_ident = |a: &syn::Attribute, i: &syn::Ident| {
-                    let path_to_name = |p: &syn::Path| p.segments
-                        .last()
-                        .map(|seg| seg.ident.to_string())
-                        .expect("The given path was not an identifier.");
-                    path_to_name(&a.path) == i.to_string()
-                };
-                let attributes = match &mut item {
-                    syn::Item::Const(i) => &mut i.attrs,
-                    syn::Item::Enum(i) => &mut i.attrs,
-                    syn::Item::ExternCrate(i) => &mut i.attrs,
-                    syn::Item::Fn(i) => &mut i.attrs,
-                    syn::Item::ForeignMod(i) => &mut i.attrs,
-                    syn::Item::Impl(i) => &mut i.attrs,
-                    syn::Item::Macro(i) => &mut i.attrs,
-                    syn::Item::Macro2(i) => &mut i.attrs,
-                    syn::Item::Mod(i) => &mut i.attrs,
-                    syn::Item::Static(i) => &mut i.attrs,
-                    syn::Item::Struct(i) => &mut i.attrs,
-                    syn::Item::Trait(i) => &mut i.attrs,
-                    syn::Item::TraitAlias(i) => &mut i.attrs,
-                    syn::Item::Type(i) => &mut i.attrs,
-                    syn::Item::Union(i) => &mut i.attrs,
-                    syn::Item::Use(i) => &mut i.attrs,
-                    _ => panic!("Could not extract attributes")
-                };
-
-                let attribute_index = attributes.iter()
-                    .enumerate()
-                    .find(|(_, a)| attribute_has_ident(a, &ident))
-                    .expect("Could not find expected attribute").0;
-                attributes.remove(attribute_index)
-            };
-
-            // Transform the attribute to AttributeArgs
-            let attr_args = {
-                match attribute.parse_meta().unwrap() {
-                    syn::Meta::List(list) => list.nested.into_iter().collect(),
-                    _ => vec![]
-                }
-            };
-
-            // Execute the attribute function
-            let implementation_ts = $attr(attr_args, quote::quote! { #item });
-
-            // Assert that both token streams are equal
-            let remove_whitespace = |s: String| s.chars()
-                .filter(|c| !c.is_whitespace())
-                .collect::<String>();
-            assert_eq!(remove_whitespace(implementation_ts.to_string()), remove_whitespace(expected_ts.to_string()))
+            compare_implementations(|args, ts| $attr(args, ts), ident, item, expected_ts)
         }
+    }
+}
+
+pub fn compare_implementations(
+    implementor: fn(AttributeArgs, TokenStream2) -> TokenStream2,
+    attribute_ident: Ident,
+    mut item: Item,
+    expectation: TokenStream2,
+) {
+    let attribute = extract_attribute_from_item(&attribute_ident, &mut item);
+    let attribute_args = transform_attribute_to_attribute_args(attribute);
+    let implementation = (implementor)(attribute_args, quote! {#item});
+    let remove_whitespace = |s: String| s.chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>();
+    assert_eq!(remove_whitespace(implementation.to_string()), remove_whitespace(expectation.to_string()))
+
+}
+
+fn extract_attribute_from_item(attribute_ident: &Ident, item: &mut Item) -> Attribute {
+    let attributes = get_attributes_from_item(item);
+    let attribute_index = attributes.iter()
+        .enumerate()
+        .find(|(_, a)| attribute_has_ident(a, attribute_ident))
+        .expect("Could not find expected attribute").0;
+    attributes.remove(attribute_index)
+}
+
+fn get_attributes_from_item(item: &mut Item) -> &mut Vec<Attribute> {
+    match item {
+        Item::Const(i) => &mut i.attrs,
+        Item::Enum(i) => &mut i.attrs,
+        Item::ExternCrate(i) => &mut i.attrs,
+        Item::Fn(i) => &mut i.attrs,
+        Item::ForeignMod(i) => &mut i.attrs,
+        Item::Impl(i) => &mut i.attrs,
+        Item::Macro(i) => &mut i.attrs,
+        Item::Macro2(i) => &mut i.attrs,
+        Item::Mod(i) => &mut i.attrs,
+        Item::Static(i) => &mut i.attrs,
+        Item::Struct(i) => &mut i.attrs,
+        Item::Trait(i) => &mut i.attrs,
+        Item::TraitAlias(i) => &mut i.attrs,
+        Item::Type(i) => &mut i.attrs,
+        Item::Union(i) => &mut i.attrs,
+        Item::Use(i) => &mut i.attrs,
+        _ => panic!("Could not extract attributes")
+    }
+}
+
+fn attribute_has_ident(a: &Attribute, i: &Ident) -> bool {
+    *i == path_to_name(&a.path)
+}
+
+fn path_to_name(p: &Path) -> String {
+    p.segments
+        .last()
+        .map(|seg| seg.ident.to_string())
+        .expect("The given path was not an identifier.")
+}
+
+fn transform_attribute_to_attribute_args(attribute: Attribute) -> AttributeArgs {
+    match attribute.parse_meta().unwrap() {
+        syn::Meta::List(list) => list.nested.into_iter().collect(),
+        _ => vec![]
     }
 }
 
@@ -122,6 +137,8 @@ mod tests {
 
     #[test]
     fn foo() {
+        use crate::compare_implementations;
+
         assert_attribute_implementation_as_expected!(
             crate::tests : bar,
             item: {
